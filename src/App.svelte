24501e841ef974793b8179942bca8ba16ca718e2
Robert <70626111+Robert95Sch@users.noreply.github.com>
2021-09-14 14:33:57 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
 <main>
   <h1>Svelte + Node.js API</h1>
   <h2>
-    Deployed with
+    Deployed with changed text
     <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
       Vercel
     </a>
